4e4154bb8556a16cd7adb635fb240f3161093055
jigowatt <jigowatt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-05 20:37:36 +0000

    Deploys version 1.6
    git-svn-id: https://plugins.svn.wordpress.org/jigoshop/trunk@676619 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/write-panels/order-data.php
+++ admin/write-panels/order-data.php
@@ -13,7 +13,7 @@
  * @package             Jigoshop
  * @category            Admin
  * @author              Jigowatt
- * @copyright           Copyright © 2011-2012 Jigowatt Ltd.
+ * @copyright           Copyright © 2011-2013 Jigowatt Ltd.
  * @license             http://jigoshop.com/license/commercial-edition
  */
 
@@ -116,7 +116,7 @@ function jigoshop_order_data_meta_box($post) {
 
 		<div id="order_customer_billing_data" class="panel jigoshop_options_panel">
             <?php
-            //display billing fieds and values
+				// display billing fields and values
 
                 $billing_fields = array(
 					'company'   => __('Company', 'jigoshop'),
@@ -132,6 +132,18 @@ function jigoshop_order_data_meta_box($post) {
 					'email'     => __('Email Address', 'jigoshop'),
 				);
 
+				if ( isset( $data['billing_euvatno'] )) {
+					$start = $billing_fields;
+					array_splice( $start, 1 );
+					$end = $billing_fields;
+					array_splice( $end, 0, 1 );
+					$billing_fields = array_merge(
+						$start,
+						array( 'euvatno' => __('EU VAT Number', 'jigoshop') ),
+						$end
+					);
+				}
+				
                 foreach($billing_fields as $field_id => $field_desc) {
                     $field_id = 'billing_' . $field_id;
                     $field_value = '';
@@ -452,6 +464,7 @@ function jigoshop_order_totals_meta_box($post) {
 			}
 			$payment_select .= "</select>";
 		?>
+		<?php do_action( 'jigoshop_admin_order_totals_after_shipping', $post->ID ) ?>
 		<li>
 			<label><?php _e('Total:', 'jigoshop'); ?></label>
             <input type="text" id="order_total" name="order_total" placeholder="0.00" value="<?php echo esc_attr( $_order->get_value_from_data('order_total') ); ?>" class="first" /> <?php echo $payment_select; ?>
--- shortcodes/my_account.php
+++ shortcodes/my_account.php
@@ -11,7 +11,7 @@
  * @package             Jigoshop
  * @category            Customer
  * @author              Jigowatt
- * @copyright           Copyright © 2011-2012 Jigowatt Ltd.
+ * @copyright           Copyright © 2011-2013 Jigowatt Ltd.
  * @license             http://jigoshop.com/license/commercial-edition
  */
 
@@ -45,7 +45,7 @@ function jigoshop_my_account( $atts ) {
 		<h2><?php _e('Available downloads', 'jigoshop'); ?></h2>
 		<ul class="digital-downloads">
 			<?php foreach ($downloads as $download) : ?>
-				<li><?php if (is_numeric($download['downloads_remaining'])) : ?><span class="count"><?php echo $download['downloads_remaining'] . _n(' download Remaining', ' downloads Remaining', $download['downloads_remaining'], 'jigoshop'); ?></span><?php endif; ?> <a href="<?php echo esc_url( $download['download_url'] ); ?>"><?php echo $download['download_name']; ?></a></li>
+				<li><?php if (is_numeric($download['downloads_remaining'])) : ?><span class="count"><?php echo $download['downloads_remaining'] . _n(' download Remaining', ' downloads Remaining', 'jigoshop'); ?></span><?php endif; ?> <a href="<?php echo esc_url( $download['download_url'] ); ?>"><?php echo $download['download_name']; ?></a></li>
 			<?php endforeach; ?>
 		</ul>
 		<?php endif; ?>
@@ -418,6 +418,9 @@ function jigoshop_view_order() {
                     <td><?php echo $order->get_shipping_to_display(); ?></small></td>
                 </tr><?php
             endif;
+			
+            do_action('jigoshop_processing_fee_after_shipping');
+			
             if ($jigoshop_options->get_option('jigoshop_tax_after_coupon') == 'yes' && $order->order_discount > 0) : ?><tr class="discount">
                 <td colspan="3"><?php _e('Discount', 'jigoshop'); ?></td>
                 <td>-<?php echo jigoshop_price($order->order_discount); ?></td>
